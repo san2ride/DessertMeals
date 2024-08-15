0715a89e40452d8c32ca3eb8edd266ed410b4111
Jason <sanchezjason@me.com>
2024-08-15 14:39:59 -0600

    swiftui/fetch/dessert_meals/fetch_dessert- DessertListView

---
+++ FetchDessert/FetchDessert/FetchDessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchDessertApp.swift
+//  FetchDessert
+//
+//  Created by Jason Sanchez on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchDessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            DessertListView()
+        }
+    }
+}
---
+++ FetchDessert/FetchDessert/Views/DessertListView.swift
@@ -0,0 +1,18 @@
+//
+//  DessertListView.swift
+//  FetchDessert
+//
+//  Created by Jason Sanchez on 8/15/24.
+//
+
+import SwiftUI
+
+struct DessertListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DessertListView()
+}
